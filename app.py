9eb9de8d8640319b5acbb5c9de97ab5aa3241cce
manue <manuelmhernandez37@gmail.com>
2026-02-19 18:48:42 -0600

    modificar 19-02-2026

--- app.py
+++ app.py
@@ -1,27 +1,75 @@
+import re
+from wtforms.validators import email
 from flask import Flask, render_template
+from flask import request
 from flask import flash
+from flask import redirect, url_for
 from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
 from flask import g
 
 from models import db
 from models import Alumnos
+import forms
 
 app = Flask(__name__)
 app.config.from_object(	DevelopmentConfig)
 db.init_app(app)
 csrf=CSRFProtect()
 
-@app.route("/")
-@app.route("/index")
-def index():
-	return render_template("index.html")
-@app.route("/alumnos")
-def alumnos():
-	return render_template("alumnos.html")
 @app.errorhandler(404)
 def notFound(e):
 	return render_template("404.html"),404
+
+@app.route("/", methods=['GET','POST'])
+@app.route("/index")
+def index():
+	create_form=forms.UserForm2(request.form)
+	#tem=Alumnos.query('select * from alumnos')
+	alumno= Alumnos.query.all()
+	return render_template("index.html", form=create_form,alumno=alumno)
+
+
+@app.route("/alumnos", methods=['GET','POST'])
+def alumnos():
+	create_form=forms.UserForm2(request.form)
+	if request.method=="POST":
+		alum=Alumnos(nombre=create_form.nombre.data,
+			   apaterno=create_form.apaterno.data,
+			   email=create_form.correo.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for("index"))
+	return render_template("alumnos.html",form=create_form)
+
+@app.route("/modificar", methods=['GET','POST'])
+def modificar():
+	create_form=forms.UserForm2(request.form)
+	if request.method=="POST":
+		id=request.args.get('id')
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		alum1.id=id
+		alum1.nombre=str.rsplit(create_form.nombre.data)
+		alum1.apaterno=str.rsplit(create_form.apaterno.data)
+		alum1.email=str.rsplit(create_form.email.data)
+		db.session.add(alum1)
+		db.session.commit()
+		return redirect(url_for("index"))
+	return render_template("alumnos.html",form=create_form)
+
+@app.route("/detalles", methods=["GET","POST"])
+def detalles():
+	create_form=forms.UserForm2(request.form)
+	nombre=""
+	apaterno=""
+	email="email"
+	if request.method=="GET":
+		id=request.args.get('id')
+		alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		nombre=alum1.nombre
+		apaterno=alum1.apaterno
+		email=alum1.email
+	return render_template("detalles.html",nombre=nombre,apaterno=apaterno,email=email)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
--- forms.py
+++ forms.py
@@ -3,11 +3,11 @@ from wtforms import StringField, IntegerField,PasswordField, RadioField
 from wtforms import EmailField
 from wtforms import validators
 
-class UserForm(Form):
+class UserForm2(Form):
     matricula=IntegerField('id',[validators.data_required(message="el campo es requerido"),
                                         validators.number_range(min=1,max=1000,message="el campo es requerido")])
     nombre=StringField('nombre',[validators.data_required(message="el campo es requerido"), 
                                  validators.length(min=3,max=10,message="el campo es requerido")])
     apaterno=StringField('apaterno',[validators.data_required(message="el campo es requerido"),validators.length(min=3,max=10,message="el campo es requerido")])
-    correo=EmailField('correo',[validators.data_required(message="el campo es requerido"),validators.length(min=3,max=10,message="el campo es requerido")])
+    correo=EmailField('correo',[validators.data_required(message="el campo es requerido"),validators.length(min=3,max=30,message="el campo es requerido")])
     
